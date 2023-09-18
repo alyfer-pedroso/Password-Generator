24f8a1d792378d7b343b3ab4ef44e230eb943ca1
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-09-18 18:57:56 -0300

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -3,12 +3,14 @@ const $password = document.getElementById("password");
 const $generateBtn = document.getElementById("generate");
 const $length = document.getElementById("length");
 const $showLength = document.getElementById("rangeValue");
+const $checkboxs = document.querySelectorAll("input[type='checkbox']");
 
 //*Variaveis
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
 const numbers = "0123456789";
 const symbols = "!@#$%&*(){}[]^~,.;<>?/|=+-";
+let generate = [];
 
 //*Funções
 function getUpper() {
@@ -24,10 +26,32 @@ function getSym() {
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+function checkboxs(UPPER, LOWER, NUM, SYM) {
+    if ($checkboxs[0].checked) {
+        generate.push(UPPER);
+    }
+    if ($checkboxs[1].checked) {
+        generate.push(LOWER);
+    }
+    if ($checkboxs[2].checked) {
+        generate.push(NUM);
+    }
+    if ($checkboxs[3].checked) {
+        generate.push(SYM);
+    }
+}
+
 function generatePass(getUpper, getLower, getNum, getSym) {
     let password = "";
     let passLength = $length.value;
-    const generate = [getUpper, getLower, getNum, getSym];
+    generate = [];
+
+    checkboxs(getUpper, getLower, getNum, getSym);
+
+    if (generate.length == 0) {
+        return;
+    }
+
     for (i = 0; i < passLength; i = i + generate.length) {
         generate.forEach(() => {
             let value = generate[Math.floor(Math.random() * generate.length)]();
